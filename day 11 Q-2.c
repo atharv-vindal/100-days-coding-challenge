6a01ef152608d08dc663b5f8bcc875fe0a267a16
atharv-vindal <atharv.34487@stu.upes.ac.in>
2026-08-19 23:30:27 +0000

    100 days of coding challenge day 11

---
+++ day 11 Q-2.c
@@ -0,0 +1,34 @@
+//Q22: Write a program to find profit or loss percentage given cost price and selling price.
+
+/*
+Sample Test Cases:
+Input 1:
+1000 1200
+Output 1:
+Profit 20%
+
+Input 2:
+1000 800
+Output 2:
+Loss 20%
+
+Input 3:
+1000 1000
+Output 3:
+No Profit No Loss
+
+*/
+
+#include<stdio.h>
+int main()
+{
+    int cp,sp;
+    printf("Enter cost price and selling price : ");
+    scanf("%d %d",&cp,&sp);
+    if (cp>sp)
+      printf("loss : %d%%",((cp-sp)*100)/cp);
+    else if (cp<sp)
+      printf("Profit : %d%%",((sp-cp)*100)/cp);
+    else 
+      printf("No profit no loss");
+}
